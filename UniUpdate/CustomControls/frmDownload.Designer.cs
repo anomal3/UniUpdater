e4e67744e8955b0d28c5225ccf44a0b9a0d748f1
Roman <anomal3@yandex.ru>
2022-08-09 22:39:15 +0400

    Добавьте файлы проекта.

---
+++ UniUpdate/CustomControls/frmDownload.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace UniUpdate.CustomControls
+{
+    partial class frmDownload
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prBar = new System.Windows.Forms.ProgressBar();
+            this.lblSize = new UniUpdate.CustomControls.TransparentLabel();
+            this.lblPersentage = new System.Windows.Forms.Label();
+            this.lblFileProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // prBar
+            // 
+            this.prBar.Location = new System.Drawing.Point(0, 8);
+            this.prBar.Name = "prBar";
+            this.prBar.Size = new System.Drawing.Size(820, 18);
+            this.prBar.TabIndex = 0;
+            // 
+            // lblSize
+            // 
+            this.lblSize.BackColor = System.Drawing.Color.Transparent;
+            this.lblSize.Location = new System.Drawing.Point(296, 11);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Opacity = 0;
+            this.lblSize.Size = new System.Drawing.Size(251, 12);
+            this.lblSize.TabIndex = 1;
+            this.lblSize.Text = "20MB\'s \\ 1000MB\'s";
+            this.lblSize.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblSize.TransparentBackColor = System.Drawing.Color.Transparent;
+            // 
+            // lblPersentage
+            // 
+            this.lblPersentage.Location = new System.Drawing.Point(784, 29);
+            this.lblPersentage.Name = "lblPersentage";
+            this.lblPersentage.Size = new System.Drawing.Size(33, 21);
+            this.lblPersentage.TabIndex = 2;
+            this.lblPersentage.Text = "100%";
+            // 
+            // lblFileProgress
+            // 
+            this.lblFileProgress.AutoEllipsis = true;
+            this.lblFileProgress.Location = new System.Drawing.Point(3, 29);
+            this.lblFileProgress.Name = "lblFileProgress";
+            this.lblFileProgress.Size = new System.Drawing.Size(775, 20);
+            this.lblFileProgress.TabIndex = 3;
+            this.lblFileProgress.Text = "https://www.blueboxproduction.ru/";
+            // 
+            // frmDownload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblFileProgress);
+            this.Controls.Add(this.lblPersentage);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.prBar);
+            this.Name = "frmDownload";
+            this.Size = new System.Drawing.Size(820, 49);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar prBar;
+        private TransparentLabel lblSize;
+        private System.Windows.Forms.Label lblPersentage;
+        private System.Windows.Forms.Label lblFileProgress;
+    }
+}
---
+++ UniUpdate/CustomControls/frmDownload.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniUpdate.CustomControls
+{
+    public partial class frmDownload : UserControl
+    {
+        public frmDownload(string[] args)
+        {
+            InitializeComponent();
+            Start(args);
+        }
+
+        #region Variables
+
+        string urlBase;
+
+        string fileStart;
+
+        List<string> downloadFiles = new List<string>();
+
+        WebClient wc = new WebClient();
+
+        Random random = new Random();
+
+        #endregion
+
+        /// <summary>
+        /// Afther Initialize args
+        /// </summary>
+        /// <param name="args"></param>
+        public async void Start(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+
+                foreach (var arg in args)
+                {
+
+                    var input = arg.Remove(0, 1);
+
+                    string InArgs = input.Substring(input.IndexOf('|') + 1).Replace(" ", "");
+                    input = input.Substring(0, input.LastIndexOf('|'));
+
+                    switch (input)
+                    {
+                        case "link":
+                            urlBase =  await new WebClient().DownloadStringTaskAsync(new Uri(InArgs));
+                            break;
+                        case "FS":
+                            fileStart = InArgs;
+                            break;
+                    }
+                }
+
+                Thread th = new Thread(download);
+                th.Start();
+            }
+        }
+
+
+        internal async void download()
+        {
+            if (!string.IsNullOrWhiteSpace(urlBase))
+                downloadFiles = urlBase.Split('\n').ToList();
+            else
+            {
+                MessageBox.Show("There is no argument to load.\r\nThe App will close!", "Start error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ExitCode();
+            }
+
+            
+            wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(Downloader_DownloadProgressChanged);
+
+            try
+            {
+
+                label1.Invoke(new Action(() => label1.Text = downloadFiles[i]));
+                var Name = downloadFiles[i];
+                string _chk = Name.Substring(Name.LastIndexOf("/") + 1);
+
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                wc.DownloadFileAsync(new Uri(downloadFiles[i] + "?random=" + random.Next().ToString()), Environment.CurrentDirectory + @"/" + _chk);
+
+                wc.DownloadFileCompleted += (args, e1) =>
+                {
+                    ++i;
+                    if (i < downloadFiles.Count) download();
+                    else
+                    {
+                        File.Delete("link.txt");
+                        Application.Exit();
+                    }
+                };
+            }
+            catch { }
+        }
+
+        void Downloader_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+
+            lblSpeed.Invoke(new Action(() => lblSpeed.Text = string.Format("{0} kb/s", (e.BytesReceived / 1024 / sw.Elapsed.TotalSeconds).ToString("0.00"))));
+            progressBar1.Invoke(new Action(() => progressBar1.Value = e.ProgressPercentage));
+            lblPerc.Invoke(new Action(() => lblPerc.Text = e.ProgressPercentage.ToString() + "%"));
+
+            lblPerc.Invoke(new Action(() => lblDownload.Text = string.Format("{0} MB's / {1} MB's",
+                (e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+                (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"))));
+
+        }
+
+        private void ExitCode() { System.Diagnostics.Process.Start(fileStart); Application.Exit(); }
+    }
+}
